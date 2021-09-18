b3fc32d29b36da6f5b318baa9f70c2a924b95ade
Eugene Chung <ycechung@myseneca.ca>
2021-09-18 07:32:36 -0400

    finished bubbleSort for Rust!

---
+++ bubbleSort/src/main.rs
@@ -0,0 +1,21 @@
+fn bubble_sort(items: &mut [i32]) {
+    let length = items.len();
+
+    for i in (1..length).rev() {
+        for j in 0..i {
+            if items[j] > items[j + 1] {
+                items.swap(j, j + 1);
+            }
+        }
+    }
+}
+
+fn main(){
+    let mut array1 = [5, 9, 7, 8, 1, 2, 12, 1];
+    println!("\n- Before Sort | Implementation -");
+    println!("{:?}", array1);
+    println!("- After Sort | Implementation -");
+    bubble_sort(&mut array1);
+    println!("{:?}", array1);
+    println!("\n");
+}
